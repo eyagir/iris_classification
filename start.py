eaf5e223bc2beb6619c6fdb8c8750becf4cf3d06
eyagir <eyup@live.unc.edu>
2019-10-30 15:29:52 -0400

    init commit

---
+++ start.py
@@ -0,0 +1,9 @@
+# Iris Classification Model: Machine learning model that will allow us to 
+# classify species of iris flowers. This applicaiton will introduce many
+# rudimentary features and concepts of machine learning and is a good use 
+# case for these types of models.
+
+# Use case: Botanist wants to determine the species of an iris flower based on
+# charateristics of that flower. For instance attributes including petal
+# length, width, etc. are the "features" that determine the classification of a
+# given iris flower.
